b1bc798293ce49bfbd499aedf0610919e094e3d2
Nathnael Alemu <nathnael.alemu@yahoo.com>
2017-08-04 18:26:24 -0500

    redesigned UI of the game in accordance with material design

--- ScarnesDice/app/src/main/java/com/natialemu/scarnesdice/View/ScarnesActivity.java
+++ ScarnesDice/app/src/main/java/com/natialemu/scarnesdice/View/ScarnesActivity.java
@@ -1,11 +1,18 @@
 package com.natialemu.scarnesdice.View;
 
+import android.content.Intent;
 import android.media.Image;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.natialemu.scarnesdice.Model.ScarnesGame;
 import com.natialemu.scarnesdice.Model.ScarnesGameImpl;
@@ -16,15 +23,110 @@ public class ScarnesActivity extends AppCompatActivity implements UiUpdateListne
     ScarnesGame game;
     int dice1 = 1;
     int dice2 = 1;
+    private LinearLayout layoutFabRoll;
+
+    //Linear layout holding the Edit submenu
+    private LinearLayout layoutFabHold;
+
+    private LinearLayout layoutFabReset;
+    private boolean fabExpanded = false;
+
+    private FloatingActionButton fabMain;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scarnes);
+        Toolbar gameToolbar = (Toolbar)findViewById(R.id.toolbar);
+
+        setSupportActionBar(gameToolbar);
+
+        fabMain = (FloatingActionButton) findViewById(R.id.mainFab);
+
+        layoutFabRoll = (LinearLayout) findViewById(R.id.layoutFabRoll);
+        layoutFabHold  =(LinearLayout) findViewById(R.id.layoutFabHold);
+        layoutFabReset = (LinearLayout) findViewById(R.id.layoutFabReset);
+
+        fabMain.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(fabExpanded){
+                    closeSubMenusFab();
+                }else{
+                    openSubMenusFab();
+                }
+
+            }
+        });
+
+
+        FloatingActionButton rollFab = (FloatingActionButton)findViewById(R.id.fabRoll);
+
+        FloatingActionButton holdFab = (FloatingActionButton)findViewById(R.id.fabHold);
+
+        FloatingActionButton resetFab = (FloatingActionButton)findViewById(R.id.fabReset);
+
+        rollFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(fabExpanded){
+                    onRollClick(view);
+                }
+            }
+        });
+        holdFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(fabExpanded){
+                    onHoldClick(view);
+                }
+            }
+        });
+
+        resetFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(fabExpanded){
+                    onResetClick(view);
+                }
+            }
+
+
+        });
+
+        closeSubMenusFab();
+
         game = new ScarnesGameImpl();
         game.setuiListner(this);
         game.initUi();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if(id == R.id.game_help){
+            Intent intent = new Intent(this, HelpActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
@@ -108,9 +210,11 @@ public class ScarnesActivity extends AppCompatActivity implements UiUpdateListne
 
     @Override
     public boolean updateTextViewUi(String message) {
-        TextView view = (TextView)findViewById(R.id.notification);
 
-        view.setText(message);
+        if (message.equals("")){
+            return true;
+        }
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
 
         return true;
 
@@ -121,16 +225,36 @@ public class ScarnesActivity extends AppCompatActivity implements UiUpdateListne
         dice1 = (int)(Math.random()*6) + 1;
         dice2 = (int)(Math.random()*6) + 1;
         game.roll(dice1,dice2);
+        closeSubMenusFab();
 
     }
 
     public void onHoldClick(View view) {
         game.hold(dice1,dice2);
+        closeSubMenusFab();
     }
 
     public void onResetClick(View view) {
 
         game.reset();
+        closeSubMenusFab();
 
+    }
+
+    private void closeSubMenusFab(){
+        layoutFabHold.setVisibility(View.INVISIBLE);
+        layoutFabReset.setVisibility(View.INVISIBLE);
+        layoutFabRoll.setVisibility(View.INVISIBLE);
+        fabMain.setBackgroundResource(R.drawable.ic_add_black_24dp);
+        fabExpanded = false;
+    }
+
+    //Opens FAB submenus
+    private void openSubMenusFab() {
+        layoutFabHold.setVisibility(View.VISIBLE);
+        layoutFabRoll.setVisibility(View.VISIBLE);
+        layoutFabReset.setVisibility(View.VISIBLE);
+        fabMain.setBackgroundResource(R.drawable.ic_close_black_24dp);
+        fabExpanded = true;
     }
 }
